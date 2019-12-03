7076d04baabcd1948dc3534e3115f091c2c0212a
richard <r50206v@gmail.com>
2019-12-02 20:46:12 -0500

    [BUGFIX] rename file

---
+++ homework5/prob1-a_result.sol
@@ -0,0 +1,18 @@
+Academic license - for non-commercial use only
+
+Gurobi Optimizer version 8.1.1 build v8.1.1rc0 (mac64)
+Copyright (c) 2019, Gurobi Optimization, LLC
+
+Read LP format model from file prob1-a.lp
+Reading time = 0.00 seconds
+: 9 rows, 20 columns, 40 nonzeros
+Optimize a model with 9 rows, 20 columns and 40 nonzeros
+Coefficient statistics:
+  Matrix range     [1e+00, 1e+00]
+  Objective range  [2e+01, 2e+03]
+  Bounds range     [1e+01, 9e+03]
+  RHS range        [2e+02, 4e+03]
+Presolve time: 0.00s
+
+Solved in 0 iterations and 0.00 seconds
+Infeasible model
